eaeec90101738a6b84eda35efd8fd33a0f007668
lily <chang12li24@163.com>
2017-08-08 18:21:30 +0800

    Add theme

--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,35 +1,28 @@
+'use strict'
+import util from "../../utils/util.js";
+import config from "../../config.js";
 Page({
   data: {
-    text: "This is theme page."
+    text: "This is theme page.",
+    bookData:[],
+    searchType: 'keyword',
+    hotKeyword: ['0-3岁', '权志郎', '冰雪奇缘'],
+    ageKeyword: ['0-2岁', '3-6岁', '7-9岁', '9岁以上'],
+    langKeyword: ['中文', '英文',],
+    themeTag: ['交通工具', '情绪管理', '友情', '亲情', '入园', '性教育', '无字书', '音乐', '古诗词', '儿歌', '建筑', '幽默', '科普'],
   },
   onLoad: function(options) {
-    // Do some initialize when page load.
+    var self = this;
+    let url = config.getBooksList;
+    let successFuc = function (res) {
+      console.log("success");
+      self.setData({
+        bookData: res.data.data
+      })
+      console.log(res.data.data);
+    };
+    util.doGet(url, successFuc);
   },
-  onReady: function() {
-    // Do something when page ready.
-  },
-  onShow: function() {
-    // Do something when page show.
-  },
-  onHide: function() {
-    // Do something when page hide.
-  },
-  onUnload: function() {
-    // Do something when page close.
-  },
-  onPullDownRefresh: function() {
-    // Do something when pull down.
-  },
-  onReachBottom: function() {
-    // Do something when page reach bottom.
-  },
-  onShareAppMessage: function () {
-   // return custom share data when user share.
-  },
-  onPageScroll: function() {
-    // Do something when page scroll
-  },
-  // Event handler.
   viewTap: function() {
     this.setData({
       text: 'Set some data for updating view.'
