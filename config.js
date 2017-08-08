fde75a581f9df45185edc2a81ca65f7d9eb0345d
lily <chang12li24@163.com>
2017-08-08 16:01:45 +0800

    mine

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict'
 var host = "https://www.easy-mock.com/mock/5987d9b7a1d30433d85714de/uuShare";
 module.exports = {
-	getBooksList : `${host}/books/list`
+	getBooksList : `${host}/books/list`,
+  getStatuBooksList: `${host}/books/statuBooksList`,
 }
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,9 +1,49 @@
+'use strict'
+import util from "../../utils/util.js";
+import config from "../../config.js";
+
 Page({
   data: {
-    text: "This is my page."
+    text: "This is my page.",
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换  
+    currentTab: 0, 
+    items: []
   },
   onLoad: function(options) {
-    // Do some initialize when page load.
+    var that = this;
+    let url = config.getStatuBooksList;
+
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+    let successFuc = function (res) {
+      that.setData({
+        items: res.data.data
+      })
+    };
+    let subData = {};//获取到个人信息
+    util.doPost(url, subData, successFuc);
+  },
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+  },
+  swichNav: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   },
   onReady: function() {
     // Do something when page ready.
