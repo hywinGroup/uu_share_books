3a5a5801fc3463bb163cc7bb1d33f0a3fe546309
susie <susiewang163@163.com>
2017-08-09 16:37:52 +0800

    添加授权弹框

--- app.js
+++ app.js
@@ -2,6 +2,21 @@
 App({
   onLaunch: function(options) { 
     // Do something initial when launch.
+    wx.showModal({
+      content: '“UU摆书”要获取你的地理位置，是否允许?',
+      cancelText:"不允许",
+      cancelColor:"#0075ff",
+      confirmText:"允许",
+      confirmColor:"#0075ff",
+      success: function(res) {
+        if (res.confirm) {
+          //console.log('用户点击确定')
+          //success process TODO
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
   },
   onShow: function(options) {
       // Do something when show.
