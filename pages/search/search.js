0011822b212fdb2d2c5fd726bae7a2a032ed302c
susie <susiewang163@163.com>
2017-08-09 13:46:01 +0800

    添加search

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,7 +1,13 @@
+'use strict'
+import util from "../../utils/util.js";
+import config from "../../config.js";
+
 Page({
   data: {
     text: "This is search page.",
-    searchKey : ""
+    searchKey : "",
+    bookData:[],
+    isSearched:false
   },
   onLoad: function(options) {
     // Do some initialize when page load.
@@ -39,12 +45,28 @@ Page({
   searchBooks:function(){
     //var searchKey = document.getElementById("searchKey").value;
     var self = this;
-    console.log("searchKey:"+self.data.searchKey);
+    //console.log("searchKey:"+self.data.searchKey);
+    let successFuc = function(res){
+      self.setData({
+        bookData: res.data.data,
+        isSearched: true
+      })
+    }
+    util.doPost(config.getBooksList,{
+      searchKey:self.data.searchKey
+    },successFuc);
   },
   getInputValue:function(e){
     this.setData({
       searchKey:e.detail.value
     });
+  },
+  goToDetail:function(event){
+    var bookId = event.currentTarget.dataset.id;
+    console.log("search id:"+bookId);     
+    wx.navigateTo({
+      url: '../detail/detail?id='+bookId
+    })
   },
   customData: {
     hi: 'MINA'
